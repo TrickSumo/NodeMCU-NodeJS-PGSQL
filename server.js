3b2dff24bc24bc3af4d0600fa943ba61f1c255a4
Rishi Kumar Tiwari <rishi008.tiwari@gmail.com>
2025-03-23 11:47:41 +0530

    start server on render's default port

--- server.js
+++ server.js
@@ -92,5 +92,5 @@ app.get("/getAllData", async (req, res) => {
     }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
